2eea0bb384d9b169d20431a5456593a0422b853e
drjsite <drjsite@163.com>
2019-07-23 23:54:43 +0800

    add mock

--- src/mock/index.js
+++ src/mock/index.js
@@ -10,15 +10,13 @@ export default {
         mock.onPost('/login').reply(arg => {
             let {userName, password} = JSON.parse(arg.data);
             return new Promise((resolve) => {
-                let token = null;
                 let hasUser = LoginUsers.some(u => {
                     if (u.username === userName && u.password === password) {
-                        token = 'adminXXXXXX';
                         return true
                     }
                 });
                 if (hasUser) {
-                    resolve([200, {code: 200, msg: '请求成功', token: token}])
+                    resolve([200, {code: 200, msg: '请求成功'}])
                 } else {
                     resolve([200, {code: 500, msg: '账号或密码错误'}])
                 }
